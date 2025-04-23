9d25696fab4d24b0912529b63c2db5f311433e52
MD. MINHAZ <mdm047767@gmail.com>
2025-04-24 00:02:31 +0600

    env config and Base metadata updated

--- alembic/env.py
+++ alembic/env.py
@@ -2,19 +2,19 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from dotenv import load_dotenv
-import os
+from dbcore import get_config
+from dbcore import Base
 
 
 # Load the .env file
-load_dotenv()
+env_config = get_config()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 # Set the SQLAlchemy database URL from the environment variable loaded via .env file
-config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+config.set_main_option('sqlalchemy.url', env_config.get('DATABASE'))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -25,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
