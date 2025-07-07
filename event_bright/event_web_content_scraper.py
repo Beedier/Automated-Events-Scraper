877af1390e58c4530311f582b4537f09feaaafb9
MD. MINHAZ <mdm047767@gmail.com>
2025-07-07 16:26:41 +0000

    removed print statement

--- event_bright/event_web_content_scraper.py
+++ event_bright/event_web_content_scraper.py
@@ -83,7 +83,6 @@ def get_event_web_content_from_event_bright(
         if not ticket_cost_el:
             ticket_cost_el = soup.select_one('span.CondensedConversionBar-module__priceTag___3AnIu')
         ticket_cost = clean_text(ticket_cost_el.get_text()) if ticket_cost_el else "Free"
-        print(ticket_cost)
 
         full_address = ""
         address_container = soup.select_one('div.location-info__address')
