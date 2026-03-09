af45e2a95cecd0c2e8a717bdfa5f95f1e154ca48
MD. MINHAZ <majesticminhaz@gmail.com>
2026-03-09 22:14:51 +0600

    modified podman to docker dump_db.sh file

--- dump_db.sh
+++ dump_db.sh
@@ -8,7 +8,7 @@ OUTPUT_DIR="."
 OUTPUT_FILE="$OUTPUT_DIR/db_data_backup.sql.gz"
 
 # Get container ID of the db service
-CONTAINER_ID=$(podman ps --filter "name=${DB_SERVICE_NAME}" --format "{{.ID}}" | head -n 1)
+CONTAINER_ID=$(docker ps --filter "name=${DB_SERVICE_NAME}" --format "{{.ID}}" | head -n 1)
 
 if [ -z "$CONTAINER_ID" ]; then
   echo "❌ Database container ($DB_SERVICE_NAME) not running."
@@ -18,7 +18,7 @@ fi
 echo "📦 Creating compressed data-only database dump..."
 
 # Dump and compress
-if podman exec "$CONTAINER_ID" pg_dump --data-only -U "$DB_USER" "$DB_NAME" --exclude-table=alembic_version | gzip > "$OUTPUT_FILE"; then
+if docker exec "$CONTAINER_ID" pg_dump --data-only -U "$DB_USER" "$DB_NAME" --exclude-table=alembic_version | gzip > "$OUTPUT_FILE"; then
   echo "✅ Compressed data-only database dump successful: $OUTPUT_FILE"
 else
   echo "❌ Compressed data-only database dump failed."
